6e3c6776af09c6062f8c71aff873b6ba59f6af87
liuwin7 <liuwin7@163.com>
2015-11-27 10:16:51 +0800

    First Modify

--- GitWorkFlowDemo/ViewController.swift
+++ GitWorkFlowDemo/ViewController.swift
@@ -14,5 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func function() {
+        print("First Modify")
+    }
 }
 
